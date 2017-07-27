bb857cc7580c6c812a95b64b6b5ff83a4dda14ee
xmppjingle <rochacamargothiago@gmail.com>
2017-07-26 23:11:53 -0300

    New Version With Key

--- blocking.js
+++ blocking.js
@@ -2,19 +2,28 @@
 var http = require('http'),
     exec = require('child_process').exec;
 
-if(process.argv.length==4){
+if(process.argv.length==5){
 
   http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  var cmd = process.argv[2];
-  var child = exec(cmd, function (error, stdout, stderr) {
-  var result = '{"stdout":' + stdout + ',"stderr":"' + stderr + '"}';
-  res.end(result + '\n');
-  });
-  }).listen(parseInt(process.argv[3]));
+
+  if(req.headers.key == process.argv[3]) {
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var cmd = process.argv[2];
+    var child = exec(cmd, function (error, stdout, stderr) {
+      var result = '{"stdout":' + stdout + ',"stderr":"' + stderr + '"}';
+      res.end(result + '\n');
+      });
+  }else{
+    res.writeHead(403, {'Content-Type': 'text/plain'});
+    res.end();
+  }
+
+  }
+  ).listen(parseInt(process.argv[4]));
 
 }else{
 
-  console.log("Usage: node blocking.js COMMAND_PARAM LISTEN_PORT\n");
+  console.log("Usage: node blocking.js COMMAND_PARAM KEY LISTEN_PORT\n");
 
 }
